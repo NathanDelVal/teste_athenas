11beb73441c7897949e06d273fd2a9718877583d
NathanMad <nathan@internit.com.br>
2022-12-31 15:12:15 -0300

    update

---
+++ js/index.js
@@ -0,0 +1,56 @@
+﻿$(function () {
+    var url = "http://localhost/example/php/service.php";
+    var db = DevExpress.data.AspNet.createStore({
+        key: "id",
+        loadUrl: url,
+        insertUrl: url,
+        updateUrl: url,
+        deleteUrl: url
+    });
+    $("#gridContainer").dxDataGrid({
+        dataSource: db,
+        keyExpr: "id",
+        columns: [{
+            dataField: "FirstName",
+            fixed: true
+        }, {
+            dataField: "LastName",
+            fixed: true
+        }, {
+            dataField: "address",
+            fixed: true
+        }, {
+            dataField: "gender",
+            fixed: true
+        }, {
+            dataField: "dateOfBirth",
+            dataType: "date",
+            fixed: true
+        }],
+        // filterRow: { visible: true },
+        searchPanel: { visible: true },
+        paging: {
+            pageSize: 5
+        },
+        sorting: {
+            mode: 'multiple'
+        },
+        allowColumnReordering: true,
+        allowColumnResizing: true,
+        editing: {
+            mode: "popup",
+            allowAdding: true,
+            allowUpdating: true,
+            allowDeleting: true
+        },
+        toolbar: {
+            items: [
+                {
+                    name: "addRowButton",
+                    showText: "always",
+                },
+                "searchPanel"
+            ]
+        },
+    });
+});
